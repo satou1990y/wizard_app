831f38c8f22549d8409724a2855b963a4df6227c
satou1990y <usaakirururu@gmail.com>
2023-12-01 21:36:22 +0900

    addressテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_103332) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_01_122842) do
+  create_table "addresses", charset: "utf8", force: :cascade do |t|
+    t.integer "postal_code", null: false
+    t.text "address", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_29_103332) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
 end
