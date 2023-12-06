3aa63e17981c6278011551294363eb15f67a82be
satou1990y <usaakirururu@gmail.com>
2023-12-06 22:06:31 +0900

    住所情報登録画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  get 'home/index'
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root to: "home#index"
 end
